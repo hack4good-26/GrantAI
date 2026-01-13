6e2f959112705581d591ca025bdff6a6492d4857
felix <puahtzefoong@gmail.com>
2026-01-13 19:19:41 +0800

    Update styles

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,7 +2,7 @@ import SearchForm from "@/components/search/SearchForm";
 
 export default function DashboardPage() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center p-4 md:p-24 bg-background">
+    <div className="flex min-h-screen flex-col items-center justify-center p-4 md:p-24">
       <SearchForm />
     </div>
   );
